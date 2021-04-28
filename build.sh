b93f43a3c1c5912e029bbba4854587e67bc27c09
David Peraza <dperaza@redhat.com>
2021-04-28 15:52:40 -0400

    Changin build script to take registry as env var

--- build.sh
+++ build.sh
@@ -2,21 +2,25 @@
 
 set -e
 
+if [[ -z "$CRC_REGISTRY" ]]; then
+    CRC_REGISTRY="default-route-openshift-image-registry.apps-crc.testing"
+fi
+
 if [[ -z "$CRC_REGISTRY_PASSWORD" ]]; then
     echo "Environment Variable CRC_REGISTRY_PASSWORD is required to be able to push to test registry"
     exit 1
 fi
 
-GOOS=linux # pass darwin if in mac env
+GOOS=linux # set to darwin if in mac env
 
 CGO_ENABLED=0 GOOS=$GOOS GOARCH=amd64 GO111MODULE=on go build -v -o "ssm" -ldflags="-s -w"
 
 build_id=$(uuidgen)
 project="davptest"
 
-image_name=default-route-openshift-image-registry.apps-crc.testing/$project/ssm:$build_id
+image_name=$CRC_REGISTRY/$project/ssm:$build_id
 
 docker build -t $image_name .
 
-docker login -u kubeadmin -p $CRC_REGISTRY_PASSWORD default-route-openshift-image-registry.apps-crc.testing
+docker login -u kubeadmin -p $CRC_REGISTRY_PASSWORD $CRC_REGISTRY
 docker push $image_name
